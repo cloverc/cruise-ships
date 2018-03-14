fe969e1491c2d5a64dbd60c3261600f7bcb7aeeb
Clover Chapman <clo@cloverseeds.co.uk>
2018-03-14 14:48:48 +0000

    stage 7 test passes after DRYing up the setup

--- __tests__/Ship.test.js
+++ __tests__/Ship.test.js
@@ -1,52 +1,51 @@
-/* globals describe it expect */
+/* eslint-env jest */
 const Ship = require('../src/Ship.js');
 const Port = require('../src/Port.js');
 const Itinerary = require('../src/Itinerary.js');
 
 describe('Ship', () => {
-  it('can be instantiated', () => {
-    const port = new Port('Dover');
-    const itinerary = new Itinerary([port]);
-    const ship = new Ship(itinerary);
+  describe('with a port and itinerary', () => {
+    let ship;
+    let port;
 
-    expect(ship).toBeInstanceOf(Object);
-  });
-  it('has a current port', () => {
-    const port = new Port('Dover');
-    const itinerary = new Itinerary([port]);
-    const ship = new Ship(itinerary);
+    beforeEach(() => {
+      port = new Port('Dover');
+      const itinerary = new Itinerary([port]);
+      ship = new Ship(itinerary);
+    });
 
-    expect(ship.currentPort).toBe(port);
-  });
-  it('can set sail', () => {
-    // setup
-    const port = new Port('Dover');
-    const itinerary = new Itinerary([port]);
-    const ship = new Ship(itinerary);
-    // exercise
-    ship.setSail();
-    // verify
-    expect(ship.currentPort).toBeFalsy();
-    expect(port.ships).not.toContain(ship);
-  });
-  it('can dock at a different port', () => {
-    // setup
-    const dover = new Port('Dover');
-    const calais = new Port('Calais');
-    const itinerary = new Itinerary([dover, calais]);
-    const ship = new Ship(itinerary);
+    it('can be instantiated', () => {
+      expect(ship).toBeInstanceOf(Object);
+    });
+  
+    it('has a starting port', () => {
+      expect(ship.currentPort).toBe(port);
+    });
+    
+    it('can set sail', () => {
+      // exercise
+      ship.setSail();
+      // verify
+      expect(ship.currentPort).toBeFalsy();
+      expect(port.ships).not.toContain(ship);
+    });     
 
-    // exercise
-    ship.dock();
-    // verify
-    expect(ship.currentPort).toBe(calais);
-    expect(calais.ships).toContain(ship);
-  });
-  it('gets added to port on instantiation', () => {
-    const port = new Port('Dover');
-    const itinerary = new Itinerary([port]);
-    const ship = new Ship(itinerary);
+    it('can dock at a different port', () => {
+      // setup
+      const dover = new Port('Dover');
+      const calais = new Port('Calais');
+      const itinerary = new Itinerary([dover, calais]);
+      const ship = new Ship(itinerary);
 
-    expect(port.ships).toContain(ship);
+      // exercise
+      ship.dock();
+      // verify
+      expect(ship.currentPort).toBe(calais);
+      expect(calais.ships).toContain(ship);
+    });
+
+    it('gets added to port on instantiation', () => {
+      expect(port.ships).toContain(ship);
+    });
   });
 });
