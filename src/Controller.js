8a823eeacbbfccc33482ff788cf892c9a3a488c9
Clover Chapman <clo@cloverseeds.co.uk>
2018-03-26 21:11:10 +0100

    steps 1-3 cruise-ships-gui

---
+++ src/Controller.js
@@ -0,0 +1,27 @@
+/* eslint-env browser */
+function Controller() {
+  this.initialiseSea();
+}
+
+Controller.prototype = {
+  initialiseSea() {
+    const backgrounds = [
+      './images/water0.png',
+      './images/water1.png',
+    ];
+
+    let backgroundIndex = 0;
+
+    setInterval(() => {
+      const viewport = document.querySelector('#viewport');
+      viewport.style.backgroundImage = `url('${backgrounds[backgroundIndex]}')`;
+
+      backgroundIndex += 1;
+      if (backgroundIndex === backgrounds.length) {
+        backgroundIndex = 0;
+      }
+    }, 1000);    
+    
+  }
+}
+
