e9c9d1d1bee00469031ffe3bc066c591cc777c23
Clover Chapman <clo@cloverseeds.co.uk>
2018-04-02 13:16:36 +0100

    cruise-ships GUI step 7 completed

--- src/Controller.js
+++ src/Controller.js
@@ -1,7 +1,12 @@
-/* globals window */
+/* globals window document */
 (function exportController() {
-  function Controller() {
+  function Controller(ship) {
+    this.ship = ship;
     this.initialiseSea();
+
+    document.querySelector('#sailbutton').addEventListener('click', () => {
+      this.setSail();
+    });
   }
 
   Controller.prototype = {
@@ -36,14 +41,35 @@
         portsElement.style.width = `${portsElementWidth + 256}px`;
       });
     },
-    renderShip(ship) {
+    renderShip() {
+      const ship = this.ship;
+
       const shipPortIndex = ship.itinerary.ports.indexOf(ship.currentPort);
       const portElement = document.querySelector(`[data-port-index='${shipPortIndex}']`);
 
       const shipElement = document.querySelector('#ship');
       shipElement.style.top = `${portElement.offsetTop + 32}px`;
       shipElement.style.left = `${portElement.offsetLeft - 32}px`;
+    },
+    setSail() {
+      const ship = this.ship;
 
+      const currentPortIndex = ship.itinerary.ports.indexOf(ship.currentPort);
+      const nextPortIndex = currentPortIndex + 1;
+      const nextPortElement = document.querySelector(`[data-port-index='${nextPortIndex}']`);
+      if (!nextPortElement) {
+        return alert('End of the line!');
+      }
+      const shipElement = document.querySelector('#ship');
+      const sailInterval = setInterval(() => {
+        const shipLeft = parseInt(shipElement.style.left, 10);
+        if (shipLeft === (nextPortElement.offsetLeft - 32)) {
+          ship.dock();
+          clearInterval(sailInterval);
+        }
+
+        shipElement.style.left = `${shipLeft + 1}px`;
+      }, 20);
     },
   };
 
